9fd530b16eb4f74866d852f089d35094197b905d
Ethan Steininger <esteininger21@gmail.com>
2024-03-12 11:40:27 -0400

    monday integration

--- src/api/integrations/plugins/monday.py
+++ src/api/integrations/plugins/monday.py
@@ -1,21 +1,41 @@
-import requests
+import httpx
 import json
-
-apiKey = "YOUR_API_KEY_HERE"
-apiUrl = "https://api.monday.com/v2"
-headers = {"Authorization": apiKey}
-
-query = "{ boards (limit:5) {name id} }"
-data = {"query": query}
-
-r = requests.post(url=apiUrl, json=data, headers=headers)  # make request
-print(r.json())
+from fastapi import HTTPException
 
 
 class MondayClient:
     def __init__(self, api_key):
-        self.client = monday.MondayClient(api_key)
+        self.api_key = api_key
+        self.headers = {
+            "Content-Type": "application/json",
+            "Authorization": self.api_key,
+        }
+        self.url = "https://api.monday.com/v2"
 
-    def get_boards(self, board_ids=[]):
-        """Fetch a list of boards. Optionally, specify board IDs to fetch."""
-        return self.client.boards.fetch_boards(ids=board_ids)
+    async def _make_request(self, query):
+        try:
+            async with httpx.AsyncClient() as client:
+                response = await client.get(self.url, headers=self.headers)
+                return response.json(), response.status_code
+        except Exception as e:
+            raise HTTPException(status_code=400, detail=str(e))
+
+    async def create_webhook(
+        self, board_id, webhook_url, event, column_id, column_value
+    ):
+        query = f"""
+            mutation {{
+                create_webhook (board_id: {board_id}, url: "{webhook_url}", event: {event}, config: {{ "columnId": "{column_id}", "columnValue": {json.dumps(column_value)} }}) {{
+                    id
+                    board_id
+                }}
+            }}
+        """
+        return await self._make_request(query=json.dumps({"query": query}))
+
+
+# board_id = 1234567890  # Replace with your actual board ID
+# webhook_url = "https://www.webhooks.my-webhook/test/"  # Replace with your actual webhook URL
+# event = "change_status_column_value"  # Replace with your actual event
+# column_id = "status"  # Replace with your actual column ID
+# column_value = {"$any$": True}  # Replace with your actual column value condition
