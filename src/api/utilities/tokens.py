109d24e7cecf4e3861dae38a8b59ed26ce25efc2
Ethan Steininger <esteininger21@gmail.com>
2024-03-13 18:27:14 -0400

    tokenizer

---
+++ src/api/utilities/tokens.py
@@ -0,0 +1,13 @@
+import tiktoken
+
+
+def get_token_count(model_name, string):
+    """Returns the number of tokens in a text string."""
+    try:
+        encoding = tiktoken.encoding_for_model(model_name)
+    except KeyError:
+        print("Warning: model not found. Using cl100k_base encoding.")
+        encoding = tiktoken.get_encoding("cl100k_base")
+
+    num_tokens = len(encoding.encode(string))
+    return num_tokens
