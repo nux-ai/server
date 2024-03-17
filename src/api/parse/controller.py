79b985e3178748ab2aa19f96b80f41f84f0db217
Ethan Steininger <esteininger21@gmail.com>
2024-03-17 18:46:33 -0400

    no message

--- src/api/parse/controller.py
+++ src/api/parse/controller.py
@@ -1,29 +1,13 @@
 from fastapi import APIRouter, HTTPException, Body, Depends, Request, Path
 
-from .model import ParseFileRequest, SupportedModalities
+from .model import ParseFileRequest
 from .service import ParseHandler
 
 router = APIRouter()
 
 
-@router.post("/{modality}")
-async def parse_file(
-    request: Request,
-    request_body: ParseFileRequest,
-    modality: SupportedModalities = Path(...),
-):
-    parse_handler = ParseHandler(request.index_id)
-
-    if len(request_body.file_urls) == 0:
-        raise HTTPException(status_code=400, detail="No file_urls provided")
-
-    if len(request_body.file_urls) > 1:
-        raise HTTPException(
-            status_code=400, detail="Multiple file_urls are not supported yet"
-        )
-    try:
-        return await parse_handler.run_handler_once(
-            modality=modality, file_url=request_body.file_urls[0]
-        )
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
+# @router.post("/{modality}")
+# async def parse_file(
+#     request: Request,
+#     request_body: ParseFileRequest
+# ):
